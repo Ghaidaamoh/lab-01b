8e4b26559891fab341629549a70f663cb9788110
Ghaidaamoh <ghaidaa97mohammad@gmail.com>
2021-04-25 16:57:21 +0300

    create new page

---
+++ js/app.js
@@ -0,0 +1,17 @@
+'use strict';
+let username = prompt('please enter your name');
+console.log(username);
+alert("hi im ghaidaa from jordan " + username);
+
+let age = prompt('please enter your age');
+console.log(age);
+alert("I'm 23 years old " + username);
+
+let rose = prompt('please enter your favorite rose(hybird tea , shrub )');
+console.log(rose);
+alert("i like this rose also " + username);
+
+let num  = prompt("please enter your phone number");
+console.log(num);
+alert("we will contact you soon " + username);
+
